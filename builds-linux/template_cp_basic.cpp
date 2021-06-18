3f7e395aa011c898afee9913e92007bbd25d07d1
SAPTARSHI DAS <75030944+Saptarshidas131@users.noreply.github.com>
2021-06-18 10:03:13 +0530

    Add files via upload

---
+++ builds-linux/template_cp_basic.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef short int si;
+typedef unsigned short int us;
+typedef unsigned int ui;
+typedef long int li;
+typedef long long int ll;
+typedef unsigned long int ul;
+typedef unsigned long long int ull;
+typedef long double ld;
+typedef unsigned char uc;
+typedef wchar_t wc;
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	ui t;
+
+	cin >> t;
+
+	while(t--)
+	{
+		cout << t << " ";
+	}
+
+	return 0;
+}
